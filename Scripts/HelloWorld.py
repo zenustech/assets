316dac3bac15647c76ab7a34afd444d631e790a9
littlemine <wxlwxl1993@zju.edu.cn>
2023-07-15 05:31:00 +0800

    update script

--- Scripts/HelloWorld.py
+++ Scripts/HelloWorld.py
@@ -1,5 +1,8 @@
-def multiply(a,b):
-    print("Will compute", a, "times", b)
+import ctypes
+
+def multiply(a, b, tag, arr):
+    arr = ctypes.cast(arr, ctypes.POINTER(ctypes.c_int))
+    print("Will compute", a, "times", b, tag, "-> ||", arr[0], "||", arr[1], "||", arr[2], "||.")
     c = 0
     for i in range(0, a):
         c = c + b
